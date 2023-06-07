a0e60c55f30e1645a4da018c9f26018cd1e50057
vivekpal1 <vivekpal@duck.com>
2023-06-08 00:53:03 +0530

    veification button

---
+++ src/views/HomeView/digilockerAuthentication.tsx
@@ -0,0 +1,43 @@
+import axios from "axios";
+
+const authenticateWithDigilocker = async (
+  clientID: string,
+  clientSecret: string,
+  authToken: string
+): Promise<string | null> => {
+  try {
+    // Request an access token from the DigiLocker API
+    const tokenResponse = await axios.post(
+      "https://api.digitallocker.gov.in/public/oauth2/1/token",
+      {
+        client_id: clientID,
+        client_secret: clientSecret,
+        grant_type: "client_credentials",
+        scope: "prod",
+      }
+    );
+
+    const accessToken = tokenResponse.data.access_token;
+
+    // Get the user's name using the access token
+    const userProfileResponse = await axios.get(
+      "https://api.digitallocker.gov.in/public/rest/1/myinfo",
+      {
+        headers: {
+          Authorization: `Bearer ${accessToken}`,
+          "Content-Type": "application/json",
+          "X-Auth-Token": authToken,
+        },
+      }
+    );
+
+    const userName = userProfileResponse.data.name;
+
+    return userName;
+  } catch (error) {
+    console.error("Error authenticating with DigiLocker:", error);
+    return null;
+  }
+};
+
+export default authenticateWithDigilocker;
--- src/views/HomeView/index.tsx
+++ src/views/HomeView/index.tsx
@@ -1,47 +1,82 @@
 import Link from "next/link";
-import { FC, useMemo } from "react";
-import { ConnectionProvider, WalletProvider, useConnection, useWallet } from "@solana/wallet-adapter-react";
-import { WalletMultiButton, WalletModalProvider } from "@solana/wallet-adapter-react-ui";
-import { IdentityButton, ButtonMode, GatewayProvider } from "@civic/solana-gateway-react";
-import { Connection, clusterApiUrl } from '@solana/web3.js';
+import { FC, useMemo, useState } from "react";
+import {
+  ConnectionProvider,
+  WalletProvider,
+  useConnection,
+  useWallet,
+} from "@solana/wallet-adapter-react";
+import {
+  WalletMultiButton,
+  WalletModalProvider,
+} from "@solana/wallet-adapter-react-ui";
+import { Connection, clusterApiUrl } from "@solana/web3.js";
 import styles from "./index.module.css";
-import { PublicKey } from '@solana/web3.js';
+import { PublicKey } from "@solana/web3.js";
 import { PhantomWalletAdapter } from "@solana/wallet-adapter-phantom";
 import { WalletAdapterNetwork } from "@solana/wallet-adapter-base";
+import authenticateWithDigilocker from "./digilockerAuthentication";
 
 export const HomeView: FC = ({}) => {
   const { publicKey } = useWallet();
+  const [loading, setLoading] = useState(false);
+  const [phoneNumber, setPhoneNumber] = useState("");
+  const [otp, setOTP] = useState("");
+  const [digilockerPIN, setDigilockerPIN] = useState("");
+  const [isVerified, setIsVerified] = useState(false);
 
-  const onClick = () => {};
-  const GATEKEEPER_NETWORK = new PublicKey('ignREusXmGrscGNUesoU9mxfds9AiYTezUKex2PsZV6');
-
+  const GATEKEEPER_NETWORK = new PublicKey(
+    "ignREusXmGrscGNUesoU9mxfds9AiYTezUKex2PsZV6"
+  );
 
   const network = WalletAdapterNetwork.Devnet;
-  const endpoint = useMemo(() => process.env.REACT_APP_RPC_ENDPOINT || clusterApiUrl(network), [network]);
+  const endpoint = useMemo(
+    () => process.env.REACT_APP_RPC_ENDPOINT || clusterApiUrl(network),
+    [network]
+  );
 
-  // TODO: Initiate IdentityButton
-  const Content = () => {
-    const wallet = useWallet()
-    return <header className="App-header">
-        {wallet.connected && <IdentityButton/>}
-    </header>
-}
+  const handlePhoneNumberSubmit = () => {
+    setLoading(true);
+    // Simulating API call to send OTP to the provided phone number
+    setTimeout(() => {
+      setLoading(false);
+      // Simulating receiving OTP
+      setOTP("123456");
+    }, 2000);
+  };
 
-const Gateway = () => {
-  const { connection } = useConnection();
-  const wallet = useWallet();
-  return <GatewayProvider connection={connection} wallet={wallet} gatekeeperNetwork={new PublicKey(GATEKEEPER_NETWORK)}>
-      <Content/>
-  </GatewayProvider>
-}
-const wallets = useMemo(
-  () => [
-      new PhantomWalletAdapter(),
-      // new SolletWalletAdapter({ network }),
-  ],
-  [network]
-);
+  const handleVerificationSubmit = async () => {
+    setLoading(true);
+    setTimeout(async () => {
+      setLoading(false);
+      const isVerified = otp === "123456"; // Simulating successful OTP verification
+      if (isVerified) {
+        setIsVerified(true);
+        try {
+          const clientID = "YOUR_CLIENT_ID";
+          const clientSecret = "YOUR_CLIENT_SECRET";
+          const authToken = "YOUR_AUTH_TOKEN";
 
+          const name = await authenticateWithDigilocker(
+            clientID,
+            clientSecret,
+            authToken
+          );
+
+          if (name) {
+            console.log("Verification Successful");
+            console.log("User Name:", name);
+          } else {
+            console.log("DigiLocker Authentication Failed");
+          }
+        } catch (error) {
+          console.error("Error authenticating with DigiLocker:", error);
+        }
+      } else {
+        console.log("Verification Failed");
+      }
+    }, 2000);
+  };
 
   return (
     <div className="container mx-auto max-w-6xl p-8 2xl:px-0">
@@ -64,15 +99,9 @@ const wallets = useMemo(
           <div className="hero min-h-16 py-4">
             <div className="text-center hero-content">
               <div className="max-w-lg">
-                <h1 className="mb-5 text-5xl font-bold">
-                  Avatar
-                </h1>
-                <p className="mb-5">
-                  On Chain KYC Verification System.
-                </p>
-                <p className="mb-5">
-                  Connect your wallet to get started.
-                </p>
+                <h1 className="mb-5 text-5xl font-bold">Avatar</h1>
+                <p className="mb-5">On Chain KYC Verification System.</p>
+                <p className="mb-5">Connect your wallet to get started.</p>
                 <p>
                   {publicKey ? <>Your address: {publicKey.toBase58()}</> : null}
                 </p>
@@ -80,49 +109,98 @@ const wallets = useMemo(
             </div>
           </div>
 
-          <div className="max-w-4xl mx-auto">
-            {/* TODO: Add Civic Button */}
-            {/* <GatewayProvider
-              connection={new Connection(clusterApiUrl("devnet"))}
-              cluster="devnet"
-              gatekeeperNetwork={ GATEKEEPER_NETWORK }> */}
-              {/* TODO: Add identity button */}
-              {/* <IdentityButton mode={ButtonMode.DARK} />
-            </GatewayProvider> */}
+          <ul className="text-left leading-10">
+            {/* <li className="mb-5">
+              <Link href="/gallery">
+                <a className="text-4xl font-bold hover:underline">
+                  🏞 -- NFT Gallery
+                </a>
+              </Link>
+            </li>
+            <li className="mb-5">
+              <Link href="/mint">
+                <a className="text-4xl font-bold hover:underline">
+                  🍬 -- Candy Machine Mint UI
+                </a>
+              </Link>
+            </li> */}
+            <li>
+              {/* <Link href="/tweeter">
+                <a className="mb-5 text-4xl font-bold hover:underline">
+                  🐦 -- Solana Tweeter
+                </a>
+              </Link> */}
+            </li>
+          </ul>
 
-            <div className="App">
-            <ConnectionProvider endpoint={endpoint}>
-                    <WalletModalProvider>
-                        <Gateway />
-                    </WalletModalProvider>
-            </ConnectionProvider>
-        </div>
-            <ul className="text-left leading-10">
-              {/* <li className="mb-5"> */}
-                {/* <Link href="/gallery">
-                  <a className="text-4xl font-bold hover:underline">
-                    🏞 -- NFT Gallery
-                  </a>
-                </Link>
-              </li>
-              {/* <li className="mb-5">
-                <Link href="/mint">
-                  <a className="text-4xl font-bold hover:underline">
-                    🍬 -- Candy Machine Mint UI
-                  </a>
-                </Link>
-              </li> */}
-              <li>
-                {/* <Link href="/tweeter">
-                  <a className="mb-5 text-4xl font-bold hover:underline">
-                    🐦 -- Solana Tweeter
-                  </a>
-                </Link> */}
-              </li>
-            </ul>
-          </div>
+          {!isVerified ? (
+            <>
+              <h2>Connect Wallet and Verify</h2>
+              <p>Click the button below to start the verification process.</p>
+              {!phoneNumber ? (
+                <button onClick={handlePhoneNumberSubmit} disabled={loading}>
+                  Start Verification
+                </button>
+              ) : (
+                <>
+                  <h2>Enter Your Phone Number</h2>
+                  <input
+                    type="text"
+                    value={phoneNumber}
+                    onChange={(e) => setPhoneNumber(e.target.value)}
+                    placeholder="Phone Number"
+                  />
+                  <button onClick={handlePhoneNumberSubmit} disabled={loading}>
+                    Send OTP
+                  </button>
+                </>
+              )}
+            </>
+          ) : (
+            <>
+              <h2>Verify with Digilocker</h2>
+              <form onSubmit={handleVerificationSubmit}>
+                <label>
+                  Phone Number:
+                  <input
+                    type="text"
+                    value={phoneNumber}
+                    onChange={(e) => setPhoneNumber(e.target.value)}
+                  />
+                </label>
+                <br />
+                <label>
+                  Digilocker PIN:
+                  <input
+                    type="password"
+                    value={digilockerPIN}
+                    onChange={(e) => setDigilockerPIN(e.target.value)}
+                  />
+                </label>
+                <br />
+                {loading ? (
+                  <p>Loading...</p>
+                ) : (
+                  <button type="submit" disabled={loading}>
+                    Verify
+                  </button>
+                )}
+              </form>
+            </>
+          )}
+          <button
+            onClick={() => {
+              // Open Digilocker verification modal
+              console.log("Digilocker verification button clicked");
+            }}
+            className="btn btn-primary mt-4"
+          >
+            Verify with Digilocker
+          </button>
         </div>
       </div>
     </div>
   );
 };
+
+export default HomeView;
